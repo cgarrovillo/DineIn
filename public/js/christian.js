13bb8bd0075dde244306e913be9064ad93032b28
christianjpg <christian_prieto@yahoo.com>
2020-05-16 02:20:25 -0600

    Adds constraint validation to menu add submission

--- public/js/christian.js
+++ public/js/christian.js
@@ -1,19 +1,5 @@
 const logging = true
 debug("Begin JS Load")
-var firebase = require("firebase");
-
-// import * as firebase from 'firebase/app'
-// var firebaseConfig = {
-//     apiKey: process.env.REACT_APP_FIREBASE_KEY,
-//     authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-//     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
-//     projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-//     messagingSenderId: process.env.REACT_APP_FIREBASE_SENDERID,
-//     appId: process.env.REACT_APP_FIREBASE_APPID,
-//     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
-// }
-
 
 const input = document.querySelector('#file')
 debug(input)
---
+++ src/services/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+
+var config = {
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
+}
+
+firebase.initializeApp(config)
+export const auth = firebase.auth
+export const db = firebase.database()
+export const storage = firebase.storage()
